6f6b55f4018607a2cc19c9187cebc286866af389
noushin94 <noushin_ahmadvand@yahoo.com>
2023-04-18 18:41:35 +0330

    preprocessing

--- classifiers.py
+++ classifiers.py
@@ -1,4 +1,13 @@
 import pandas as pd
 import numpy as np
-df = pd.read_exel(r'DiabetesDiagnosis.xls')
+df = pd.read_excel(r'DiabetesDiagnosis.xls')
 df.head()
+df.isnull().sum()
+df.dtypes
+df['Diagnosis'].value_counts()
+
+X = df.drop('Diagnosis', axis=1)
+Y = df['Diagnosis']
+
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.25, random_state=22)
