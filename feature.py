c6999288f91cfe0c11e989dc0e7451f72e23d6d9
noushin94 <noushin_ahmadvand@yahoo.com>
2023-04-18 19:05:54 +0330

    libraries

--- feature.py
+++ feature.py
@@ -1,1 +1,16 @@
 
+# %tensorflow_version 2.x
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.applications.vgg16 import decode_predictions
+from keras.applications.vgg16 import VGG16
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Model
+from pickle import dump
+print(tf.__version__)
+print(keras.__version__)
